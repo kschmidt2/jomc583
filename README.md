4740bbff72693b3855fe2336ac900ed052f70f4a
kschmidt2 <k.schmidt2@gmail.com>
2016-01-19 10:17:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the master repository for JOMC 583.
---
+++ class exercises/Jan14/scripts.js
@@ -0,0 +1,10 @@
+var firstName = "Kiersten";
+var lastName = "Schmidt";
+
+function sayHi(firstName, lastName) {
+  var myVar = "Hello! My name is " + firstName + " " + lastName + ".";
+  console.log(myVar);
+  return myVar;
+}
+
+sayHi(firstName, lastName);
